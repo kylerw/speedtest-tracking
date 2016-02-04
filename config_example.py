6f1a9f5a7dedc9ae309c7632d8e0a6cc221a579e
kylerw <kylerw@gmail.com>
2016-02-04 12:44:18 -0700

    generalized for docker consumption

--- config_example.py
+++ config_example.py
@@ -1,9 +1,9 @@
 '''
-  Speed test is using: https://github.com/rsvp/speedtest-linux.git
+  https://github.com/sivel/speedtest-cli.git
 '''
 
-SPEEDTEST_FILE_LOCATION = '/mnt/cache/Apps/scripts/speedtest-linux/speedtest'
-LOG_FILE = 'speedtest.log'
-DB_FILE = 'speedtest.db'
+SPEEDTEST_FILE_LOCATION = '/config/speedtest-cli/speedtest'
+LOG_FILE = '/config/speedtest-tracking/speedtest.log'
+DB_FILE = '/config/speedtest-tracking/speedtest.db'
 
 
