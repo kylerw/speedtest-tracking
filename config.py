a8130f0ab374b3be43c7a664b16f6226d4eefcb0
Kyler Wilson <kylerw@gmail.com>
2016-02-04 10:22:49 -0700

    remove indiv files

--- config.py
+++
@@ -1,9 +0,0 @@
-'''
-https://github.com/sivel/speedtest-cli
-'''
-
-SPEEDTEST_FILE_LOCATION = '/mnt/cache/Apps/scripts/speedtest-cli/speedtest_cli.py'
-LOG_FILE = '/mnt/cache/Apps/scripts/speedtest/speedtest.log'
-DB_FILE = '/mnt/cache/Apps/scripts/speedtest/speedtest.db'
-
-
