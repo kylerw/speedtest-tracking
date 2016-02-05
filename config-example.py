fc4c40a85e244d74faeb2206c1f06fc2c48ed1bd
kylerw <kylerw@gmail.com>
2016-02-04 22:58:50 -0700

    updates for plotly

--- config-example.py
+++ config-example.py
@@ -5,5 +5,8 @@
 SPEEDTEST_FILE_LOCATION = '/config/speedtest-cli/speedtest_cli.py'
 LOG_FILE = '/config/speedtest-tracking/speedtest.log'
 DB_FILE = '/config/speedtest-tracking/speedtest.db'
-
+PLOTLY_USER = ''
+PLOTLY_API = ''
+PLOTLY_NAME = 'Broadband Speed'
+PLOTLY_PUBLIC = False
 
