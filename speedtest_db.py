797a56aef35d61f50f412176a3e2a3d7cfc18593
kylerw <kylerw@gmail.com>
2016-02-04 23:02:39 -0700

    plot with Plotly

--- speedtest_db.py
+++ speedtest_db.py
@@ -5,29 +5,33 @@ import sys
 import time
 import logging
 import config
-#import plotly.plotly as py
-#import plotly
-#from plotly.graph_objs import *
-#import pandas as pd
+import plotly.plotly as py
+import plotly
+from plotly.graph_objs import *
+import pandas as pd
 
 
 SPEEDTEST_CMD = config.SPEEDTEST_FILE_LOCATION 
 LOG_FILE = config.LOG_FILE
 DB_FILE = config.DB_FILE 
+PLOTLY_USER = config.PLOTLY_USER
+PLOTLY_API = config.PLOTLY_API
+PLOTLY_NAME = config.PLOTLY_NAME
+PLOTLY_PUBLIC = config.PLOTLY_PUBLIC
 
 def main():
   setup_logging()
   try:
     logging.info("Starting test....")
-    ping, download, upload = get_speedtest_results()
+    #ping, download, upload = get_speedtest_results()
   except ValueError as err:
     logging.info("Error ----")
     logging.info(err)
   else:
     logging.info("Test successful, results: ")
-    logging.info("%5.1f %5.1f %5.1f", ping, download, upload)
-    db_insert(ping, download, upload)
-
+    #logging.info("%5.1f %5.1f %5.1f", ping, download, upload)
+    #db_insert(ping, download, upload)
+    plotData()
 
 def setup_logging():
   logging.basicConfig(
@@ -52,16 +56,75 @@ def db_insert(ping, download, upload):
 
     try:
       sql = 'INSERT INTO data(ping, download, upload) VALUES({0}, {1}, {2})'.format(ping, download, upload)
-      '''
-      sys.stdout.write(sql)
-      sys.stdout.flush()
-      '''
       cur.execute(sql)
 
       con.commit()
     except lite.Error as err:
       logging.info("SQLite error on data insert: ")
       logging.info(err)
+
+
+def plotData():
+  con = lite.connect(DB_FILE)
+  
+  with con:
+    cur = con.cursor()
+    sql = "SELECT id, ping, download, upload, datetime(Timestamp, 'localtime') FROM data" 
+    cur.execute(sql)
+    rows = cur.fetchall()
+
+  df = pd.DataFrame( [[ij for ij in i] for i in rows] )
+  df.rename(columns={0: 'id', 1: 'Ping', 2: 'Download', 3: 'Upload', 4:'Date'}, inplace=True);
+  df = df.sort(['Date'], ascending=[1]);
+
+  trace1 = Scatter(
+     x=df['Date'],
+     y=df['Download'],
+     name='Download',
+  )
+  trace2 = Scatter(
+    x=df['Date'],
+    y=df['Upload'],
+    name='Upload',
+  )
+  trace3 = Scatter(
+    x=df['Date'],
+    y=df['Ping'],
+    name='Ping',
+    yaxis='y2'
+  )
+  layout = Layout(
+    title=PLOTLY_NAME,
+    xaxis=XAxis(
+        title='Date', 
+        autorange=True 
+    ),
+    yaxis=YAxis(
+        title='Speed (Mbps)',
+        range=[0,150],
+        type='linear',
+        autorange=False,
+        fixedrange=False,
+        ticksuffix=' (Mbps)'
+    ),
+    yaxis2=YAxis(
+        title='Ping Time (ms)',
+        range=[0,100],
+        overlaying='y',
+        side='right',
+        type='linear',
+        autorange=False,
+        ticksuffix=' (ms)'
+    ),
+  )
+
+  py.sign_in(PLOTLY_USER, PLOTLY_API)
+  data = Data([trace1,trace2,trace3])
+  fig = Figure(data=data, layout=layout)
+  py.plot(fig, filename=PLOTLY_NAME, world_readable=PLOTLY_PUBLIC)
+
+
+
 
 def get_speedtest_results():
   '''
