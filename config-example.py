37fd040f40bd3a87cfb454172f3b3f2ce0ff38ae
kylerw <kylerw@gmail.com>
2016-02-04 20:05:56 -0700

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -2,7 +2,7 @@
   https://github.com/sivel/speedtest-cli.git
 '''
 
-SPEEDTEST_FILE_LOCATION = '/config/speedtest-cli/speedtest'
+SPEEDTEST_FILE_LOCATION = '/config/speedtest-cli/speedtest.py'
 LOG_FILE = '/config/speedtest-tracking/speedtest.log'
 DB_FILE = '/config/speedtest-tracking/speedtest.db'
 
